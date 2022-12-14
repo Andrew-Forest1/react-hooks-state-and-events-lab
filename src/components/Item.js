5f80a9caa1dc342c1d8e34aee1718f1a18b5667d
Andrew <aforest41@yahoo.com>
2022-12-14 15:36:10 -0800

    Finished

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  const cartsClassName = inCart ? "in-cart" : "";
+  const buttonMessage = inCart ? "Remove From Cart" : "Add to Cart";
+  const buttonClassName = inCart ? "remove":"add" 
+  
   return (
-    <li className="">
+    <li className={cartsClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClassName} onClick={() => {setInCart(!inCart)}}>{buttonMessage}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const displayItem = (item) => {
+    if(selectedCategory === "All" || item.category === selectedCategory){
+      return <Item key={item.id} name={item.name} category={item.category} />
+    }
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={(e) => {setSelectedCategory(e.target.value)}}>
+          <option value="All" >Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items.map(displayItem)}
       </ul>
     </div>
   );
